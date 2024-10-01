ad949fbd8df7402d3f5e1de89042ec608c36f3fc
gingaminga <cjhwaan@gmail.com>
2024-10-02 00:02:28 +0900

    💄 design: [ITSPLAN-26] 둘러보기 및 구분 라인 길이 변경
    - 둘러보기 텍스트는 기본 text-sm, max-sm:text-xs 변경
    - 구분 라인 길이는 max-sm:w-48 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="w-96 max-sm:w-60">
+    <div className="w-96 max-sm:w-48">
       <Title title="로그인&회원가입" />
       <LoginForm />
       <div className="divider mb-0" />
       <div className="w-full text-right">
-        <Link href="/" className="text-base-content text-opacity-50 max-sm:text-sm">
+        <Link href="/" className="text-sm text-base-content text-opacity-50 max-sm:text-xs">
           둘러보기
         </Link>
       </div>
