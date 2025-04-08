cda43d596b4f26e6eefcf8fd51f0f1e28e6e879a
gingaminga <cjhwaan@gmail.com>
2025-04-08 23:00:34 +0900

    🎉 init: [ITP-1] 린트 추가 및 전체 설정
    - eslint-plugin-perfectionist
    - eslint-plugin-unicorn

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,12 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-essentials", "@storybook/addon-onboarding", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   staticDirs: ["../public"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 };
 export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from "eslint-plugin-perfectionist";
+import unicorn from "eslint-plugin-unicorn";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +11,33 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ...unicorn.configs.recommended,
+    rules: {
+      ...unicorn.configs.recommended.rules,
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            kebabCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+      "unicorn/import-style": ["off"],
+      "unicorn/prevent-abbreviations": ["off"],
+    },
+  },
+  {
+    ...perfectionist.configs["recommended-alphabetical"],
+    rules: {
+      ...perfectionist.configs["recommended-alphabetical"].rules,
+      "perfectionist/sort-imports": ["error", { newlinesBetween: "never" }],
+    },
+  },
+  ...compat.extends("prettier"),
+];
 
 export default eslintConfig;
